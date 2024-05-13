4f105b0a8b9e7e485b10b4d3139be5e0f2a229e6
20026216 <20026216@tafe.wa.edu.au>
2024-05-13 12:21:08 +0800

    added another stub + class progress

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,12 @@
-class CarPark:
-    pass
+class CarPark: # pascal case good for classes
+    def __init__(self,
+                 location = "Unknown",
+                 capacity = 0,
+                 plates = None,
+                 sensors = None,
+                 displays = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = []
+        self.sensors = []
+        self.displays = []
