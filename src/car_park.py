bffca9270a4a14741c1882bd460e3c963a56951e
20026216 <20026216@tafe.wa.edu.au>
2024-05-13 14:55:49 +0800

    Added register method to car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,5 @@
+from sensor import Sensor
+from display import Display
 class CarPark: ## pascal case good for identifying classes
     def __init__(self,
                  location = "Unknown",
@@ -13,3 +15,18 @@ class CarPark: ## pascal case good for identifying classes
 
     def __str__(self):         ## makes it better to represent the instance
         return f"Car Park location = {self.location} capacity = {self.capacity}. "
+
+  #  def register_sensor(self, sensor):  ## this sensor is an object, not a class
+   #     self.sensors.append(sensor)  ## This code repeats itself
+
+    #def register(self, component):          ## try writing backwards PUT THE EXCEPTIONS FIRST
+    #    if isinstance(component, Sensor):   ## writen on the next part
+    #        self.sensors.append(component)
+    #    elif isinstance(component, Display):
+    #        self.displays.append(component)
+    #    else:
+    #        raise TypeError("Invalid component type!!!")
+    def register(self, component): ## better written with exeptions first
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError(f"Invalid component type!!!")
+
