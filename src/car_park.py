5bfbee688f4d93afed994ce58ac95a3b0b097b08
20026216 <20026216@tafe.wa.edu.au>
2024-05-13 12:35:18 +0800

    Added constructors and attributes to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,4 +1,4 @@
-class CarPark: # pascal case good for classes
+class CarPark: ## pascal case good for identifying classes
     def __init__(self,
                  location = "Unknown",
                  capacity = 0,
@@ -7,6 +7,9 @@ class CarPark: # pascal case good for classes
                  displays = None):
         self.location = location
         self.capacity = capacity
-        self.plates = []
-        self.sensors = []
-        self.displays = []
+        self.plates = plates or []  ## this is better
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):         ## makes it better to represent the instance
+        return f"Car Park location = {self.location} capacity = {self.capacity}. "
