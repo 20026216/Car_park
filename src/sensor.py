ffa4b5f52a4a05104a064ad7e630340cdfa1fedc
20026216 <20026216@tafe.wa.edu.au>
2024-05-20 14:04:32 +0800

    added detection in sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -12,15 +12,27 @@ class Sensor(ABC):
 
     def _scan_plate(self):
         return "FAKE" + format(random.randint(0,999), '03d')
+
     @abstractmethod  ## everyone inheriting this will need to implement it
     def update_car_park(self, plate):
         pass
+
+    def detect_vehicle(self):
+        plate =self._scan_plate()
+        self.update_car_park()
 
     def __str__(self):
         return f'Sensor{self.id} : {"is on" if self.is_active else "is off"}'
 
 class EntrySensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming vehicle, Plate:{plate}")
 
 class ExitSensor(Sensor):
-    ...
+    def _scan_plate(self):
+        #placeholder for exit scanning
+        return random.choice(self.car_park.plates)
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing vehicle, Plate:{plate}")
