117324b973a0f8b4cc8b2893955a5e44acef5909
20026216 <20026216@tafe.wa.edu.au>
2024-05-20 12:44:55 +0800

    progress on sensor abs method

--- src/sensor.py
+++ src/sensor.py
@@ -1,4 +1,6 @@
-class Sensor:
+from abc import ABC, abstractmethod  ## decorator, like property,
+import random
+class Sensor(ABC):
     def __init__(self,
                  id,
                  car_park,
@@ -7,6 +9,12 @@ class Sensor:
         self.id = id
         self.car_park = car_park
         self.is_active = is_active
+
+    def _scan_plate(self):
+        return "FAKE" + format(random.randint(0,999), '03d')
+    @abstractmethod  ## everyone inheriting this will need to implement it
+    def update_car_park(self, plate):
+        pass
 
     def __str__(self):
         return f'Sensor{self.id} : {"is on" if self.is_active else "is off"}'
